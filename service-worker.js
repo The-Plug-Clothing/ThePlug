58f5bd41c926f09d1828ddd71e1a7dd5e5652929
The-Plug-Clothing <hunterpikari@gmail.com>
2025-10-05 13:50:31 +1300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("theplug-store").then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/icon.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
